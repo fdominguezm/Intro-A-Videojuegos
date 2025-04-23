cb4b3232532d22ca67737b95c03e99be513a1850
Francisco Dominguez <fdominguez@itba.edu.ar>
2025-04-23 19:45:19 -0300

    Charater can move and shoot in every direction

--- Assets/Scripts/Actors/CharacterInputManager.cs
+++ Assets/Scripts/Actors/CharacterInputManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class CharacterInputManager : MonoBehaviour
 {
     private WalkStrategy _walkStrategy;
+    private TurnStrategy _turnStrategy;
+
     [SerializeField] private List<Gun> _gunList;
     [SerializeField] private Gun _gun;
     [SerializeField] private KeyCode _moveUp = KeyCode.W;
@@ -15,24 +17,35 @@ public class CharacterInputManager : MonoBehaviour
     [SerializeField] private KeyCode _rifle = KeyCode.F3;
     [SerializeField] private KeyCode _attack = KeyCode.Space;
     [SerializeField] private KeyCode _reload = KeyCode.R;
-    
+
     private void Start()
     {
-        this._walkStrategy = GetComponent<WalkStrategy>();
+        _walkStrategy = GetComponent<WalkStrategy>();
+        _turnStrategy = GetComponent<TurnStrategy>();
     }
 
     private void Update()
     {
-        if (Input.GetKey(_moveUp)) _walkStrategy.Move(transform.up);
-        if (Input.GetKey(_moveDown)) _walkStrategy.Move(-transform.up);
-        if (Input.GetKey(_moveLeft)) _walkStrategy.Move(-transform.right);
-        if (Input.GetKey(_moveRight)) _walkStrategy.Move(transform.right);
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKey(_moveUp)) moveDirection += Vector3.up;
+        if (Input.GetKey(_moveDown)) moveDirection += Vector3.down;
+        if (Input.GetKey(_moveLeft)) moveDirection += Vector3.left;
+        if (Input.GetKey(_moveRight)) moveDirection += Vector3.right;
+
+        if (moveDirection != Vector3.zero)
+        {
+            moveDirection.Normalize(); // Ensures speed is consistent in diagonals
+            _walkStrategy.Move(moveDirection);
+            _turnStrategy.FaceDirection(moveDirection);
+        }
+
         if (Input.GetKey(_attack)) _gun.Attack();
         if (Input.GetKey(_reload)) _gun.Reload();
         if (Input.GetKey(_pistol)) SwitchWeapon(0);
         if (Input.GetKey(_shotgun)) SwitchWeapon(1);
         if (Input.GetKey(_rifle)) SwitchWeapon(2);
-        
+
     }
 
     private void SwitchWeapon(int weaponIndex)
--- Assets/Scripts/Strategies/Movement/TurnStrategy.cs
+++ Assets/Scripts/Strategies/Movement/TurnStrategy.cs
@@ -12,12 +12,21 @@ public class TurnStrategy : MonoBehaviour, IMovable
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void FaceDirection(Vector3 direction)
+    {
+        if (direction != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+        }
     }
 }
