da7f4432338f95b625fb46bab2a6bf398437cc86
Francisco Dominguez <fdominguez@itba.edu.ar>
2025-05-14 19:29:05 -0300

    Fixed upside down gun problem

--- Assets/Scripts/Strategies/Movement/TurnStrategy.cs
+++ Assets/Scripts/Strategies/Movement/TurnStrategy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 // [RequireComponent(typeof(Actor))]
 public class TurnStrategy : MonoBehaviour, IRotatable
@@ -6,11 +7,25 @@ public class TurnStrategy : MonoBehaviour, IRotatable
     // public float Speed => GetComponent<Actor>().ActorStats.Turn;
 
     private SpriteRenderer spriteRenderer;
+    private SpriteRenderer childSpriteRenderer;
 
-    private void Awake()
+    private List<Transform> allChildRenderers;
+
+
+private void Awake()
+{
+    spriteRenderer = GetComponent<SpriteRenderer>();
+
+    allChildRenderers = new List<Transform>
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+        transform.Find("Pistol"),
+        transform.Find("Shotgun"),
+        transform.Find("Rifle")
+    };
+
+    print(allChildRenderers);
+}
+
 
     public void FaceDirection(Vector2 direction)
     {
@@ -21,10 +36,28 @@ public class TurnStrategy : MonoBehaviour, IRotatable
         if (direction.x < 0)
         {
             spriteRenderer.flipX = true;
+
+            foreach (var t in allChildRenderers)
+            {
+                if (t.gameObject.activeInHierarchy)
+                {
+                    childSpriteRenderer = t.GetComponent<SpriteRenderer>();
+                    childSpriteRenderer.flipY = true;
+                }
+            }
         }
         else if (direction.x > 0)
         {
             spriteRenderer.flipX = false;
+
+            foreach (var t in allChildRenderers)
+            {
+                if (t.gameObject.activeInHierarchy)
+                {
+                    childSpriteRenderer = t.GetComponent<SpriteRenderer>();
+                    childSpriteRenderer.flipY = false;
+                }
+            }
         }
 
         // Ángulo de rotación (arma apuntando en dirección del movimiento)
