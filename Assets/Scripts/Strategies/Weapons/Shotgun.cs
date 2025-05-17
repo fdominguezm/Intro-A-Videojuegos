320457a6b3c4f35e3919e23a04140d258294ef60
Francisco Dominguez <fdominguez@itba.edu.ar>
2025-05-17 16:37:07 -0300

    Cinemachine and shotgun done

--- Assets/Scripts/Strategies/Weapons/Shotgun.cs
+++ Assets/Scripts/Strategies/Weapons/Shotgun.cs
@@ -3,22 +3,29 @@ using UnityEngine;
 public class Shotgun : Gun
 {
     [SerializeField] private int _bulletsPerShot => _gunStats.BulletsPerShot;
+    [SerializeField] private float spreadAngle = 30f; // Ángulo total del cono de disparo
 
     public override void Attack()
     {
         if (_bulletCount > 0 && !_isCoolingDown)
         {
             _isCoolingDown = true;
+
+            float angleStep = spreadAngle / (_bulletsPerShot - 1);
+            float startAngle = -spreadAngle / 2f;
+
             for (int i = 0; i < _bulletsPerShot; i++)
             {
-                Vector2 offset = Random.insideUnitCircle * 1.5f;
-                Vector3 spawnPos = transform.position + new Vector3(offset.x, offset.y, 0f);
+                float angleOffset = startAngle + i * angleStep;
+                Quaternion rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z + angleOffset);
 
-                GameObject bullet = Instantiate(BulletPrefab, spawnPos, transform.rotation, _bulletsParent);
+                GameObject bullet = Instantiate(BulletPrefab, transform.position, rotation, _bulletsParent);
                 bullet.GetComponent<NormalBulletStrategy>().SetOwner(this);
             }
+
             _bulletCount--;
             base.Attack();
+
             if (shootClip != null)
             {
                 audioSource.PlayOneShot(shootClip);
