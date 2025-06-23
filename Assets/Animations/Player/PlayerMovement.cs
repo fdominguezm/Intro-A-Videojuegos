ed2541e49e296ef47059a70745ab8fa4f9b1212a
gelewaut <gelewaut@itba.edu.ar>
2025-06-23 13:51:59 -0300

    Top Bot anim in player

--- Assets/Animations/Player/PlayerMovement.cs
+++ Assets/Animations/Player/PlayerMovement.cs
@@ -4,8 +4,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 0.5f;
     public Rigidbody2D rb;
+
+    [Header("Child Animators")]
+    public Animator bottomAnimator;
+    public Animator topAnimator;
+
     private Vector2 direction;
-    Animator anim;
     private Vector2 LastMoveDirection;
 
     private int weaponIndex = 0;
@@ -20,11 +24,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private KeyCode _pistol = KeyCode.Alpha1;
     [SerializeField] private KeyCode _uzi = KeyCode.Alpha2;
 
-
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
 
     void Update()
     {
@@ -44,7 +43,6 @@ public class PlayerMovement : MonoBehaviour
         {
             weaponIndex = 1;
         }
-        Debug.Log(weaponIndex);
         AnimateWeapon(weaponIndex);
     }
 
@@ -62,20 +60,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void Animate(Vector2 direction)
     {
-
-
-        anim.SetBool("isRunning", direction != Vector2.zero);
-        anim.SetFloat("MoveX", direction.x);
-        anim.SetFloat("MoveY", direction.y);
+        bottomAnimator.SetBool("isRunning", direction != Vector2.zero);
+        bottomAnimator.SetFloat("MoveX", direction.x);
+        bottomAnimator.SetFloat("MoveY", direction.y);
         if (direction != Vector2.zero)
         {
-            anim.SetFloat("LastMoveX", LastMoveDirection.x);
-            anim.SetFloat("LastMoveY", LastMoveDirection.y);
+            topAnimator.SetFloat("MoveX", direction.x);
+            topAnimator.SetFloat("MoveY", direction.y);
+            bottomAnimator.SetFloat("LastMoveX", LastMoveDirection.x);
+            bottomAnimator.SetFloat("LastMoveY", LastMoveDirection.y);
         }
     }
 
     private void AnimateWeapon(int index)
     {
-        anim.SetInteger("WeaponIdx", index);
+        topAnimator.SetFloat("Index", index);
     }
 }
