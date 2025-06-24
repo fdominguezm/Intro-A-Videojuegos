8f10270b776bf94c910750ac72d0af7f3989aea2
juampiarias <juaarias@itba.edu.ar>
2025-06-23 22:11:00 -0300

    menu de carga y how to play

---
+++ Assets/Scripts/Menu/Loading.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    public float loadingTime = 3f;
+    public string sceneToLoad = "character"; 
+
+    private async void Start()
+    {
+        Debug.Log("Inicio de pantalla de carga"); 
+        await Task.Delay((int)(loadingTime * 1000));
+        Debug.Log("Cambiando a escena: " + sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,12 +5,18 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("character"); // nombre exacto de la escena del juego
+        SceneManager.LoadScene("Loading"); // nombre exacto de la escena del juego
     }
 
     public void QuitGame()
     {
         Debug.Log("Salir del juego");
         Application.Quit();
+    }
+
+    public void HowToPlay()
+    {
+        Debug.Log("How to play");
+        SceneManager.LoadScene("Instrucciones");
     }
 }
